f17f87a379508a23df4af45c959039ed4dd8b4a7
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-05-13 15:40:24 -0700

    Signup form has custom fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :image)
+  end
 end
