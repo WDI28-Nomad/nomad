eb9a417381aaf6f9199f9ed8d99a60ffceabb382
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-14 14:25:38 -0700

    defined new and create actions for expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,11 +3,36 @@ class ExpensesController < ApplicationController
   end
 
   def new
+    @user = current_user
+    @trips = @user.trips
+    @expense = Expense.new
+  end
+
+  def create
+    @user = current_user
+    @trips = @user.trips
+    @expense = Expense.new(expense_params)
+    @trip = Trip.find(params[:id])
+    if @expense.save
+      flash[:notice] = "Expense successfully created!"
+      redirect_to user_trip(@user, @trip)
+    else
+      flash[:error] = expense.errors.full_messages.join(", ")
+      # Might change after implementing Abracadabra
+      redirect_to user_trip(@user, @trip)
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :expense_type, :amount, :start_date,
+                                    :end_date, :notes)
   end
 end
