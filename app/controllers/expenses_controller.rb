be802ba0be682190a256d41899efacdd7de15ef1
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-14 15:14:51 -0700

    can create expense and view on trips#show view

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,20 +6,22 @@ class ExpensesController < ApplicationController
     @user = current_user
     @trips = @user.trips
     @expense = Expense.new
+    @trip = Trip.find(params[:id])
   end
 
   def create
     @user = current_user
     @trips = @user.trips
     @expense = Expense.new(expense_params)
-    @trip = Trip.find(params[:id])
+    @trip = Trip.find(params[:trip_id])
     if @expense.save
+      @trip.expenses << @expense
       flash[:notice] = "Expense successfully created!"
-      redirect_to user_trip(@user, @trip)
+      redirect_to user_trip_path(@user, @trip)
     else
       flash[:error] = expense.errors.full_messages.join(", ")
       # Might change after implementing Abracadabra
-      redirect_to user_trip(@user, @trip)
+      redirect_to user_path(@user)
     end
   end
 
@@ -33,6 +35,6 @@ class ExpensesController < ApplicationController
 
   def expense_params
     params.require(:expense).permit(:name, :expense_type, :amount, :start_date,
-                                    :end_date, :notes)
+                                    :end_date, :notes, :trip_id, :user_id)
   end
 end
