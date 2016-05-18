50e4b5b7e63395fd54b9fd63d313b69ad8ff4a14
Ameer Brown <akeithbrown@gmail.com>
2016-05-18 10:49:44 -0700

    succefully merged auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-
   before_action :authenticate_user!
   before_action :set_sidebar
 
   def show
     @user = User.find_by_id(params[:id])
+    if @user != current_user
+      redirect_to root_path
+      flash[:error] = "You do not have access to this account"
+    end
     @hash = Gmaps4rails.build_markers(@trips) do |trip, marker|
       marker.lat trip.latitude
       marker.lng trip.longitude
@@ -22,16 +25,27 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find_by_id(params[:id])
+    if @user != current_user
+      redirect_to root_path
+      flash[:error] = "You do not have access to this account"
+    end
   end
 
   def update
-    @user = User.find_by_id(params[:id])
-    @user.update(user_params)
-    redirect_to user_path
+    if access?(@user.id) && @user.update(user_params)
+        flash[:notice] = "Account successfully updated"
+        redirect_to user_path
+      else
+        redirect_to welcome_index_path
+        flash[:error] = "You do not have access to this account"
+    end
   end
 
-
   private
+
+  def access?(user_id)
+    current_user ? user_id == current_user.id : false
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city, :avatar, :username)
