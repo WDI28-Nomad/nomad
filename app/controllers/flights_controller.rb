c23c2c41969b030202c2712e90ccb9057afefb0f
AdrianaVillagran <anvillagran1@gmail.com>
2016-06-08 11:47:31 -0700

    added figaro gem

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -16,7 +16,7 @@ class FlightsController < ApplicationController
   end
 
   def search
-    token = "T1RLAQLNph1SRmonSLTk6GTG6U4P6rDG6hChRfItnA8RyrHrdk1mblQQAACgJ5tAmhhlvzAU+XdcMpE2IQcmcbvyjfbRC0XGHq+7mnkWWlsHQooFrEmhct8Kc+HKBKiXYShjoPFljAOAIwklRkMv0dloQLi04FQ+Fg1gK26lZiB6rUEeobr/GfML67z5ThTfaB/OQ3UxduNc8Uar21SK5F/i3RayE7GK1Fdnbg1FiiFDPRQvyPOey0xiXZrTuTQ9BJIUuuZ1CPLpVzCN2Q**"
+    token = ENV['SABRE_TOKEN']
     headers = {
       "Authorization" => "Bearer #{token}"
     }
